b1f2b97c402a82b5af99e90e249ccff809987bc2
root <root@template.ovh>
2017-07-18 19:07:49 +0200

    Query Parameters

--- app.js
+++ app.js
@@ -46,33 +46,41 @@ request(options, function (error, response, body) {
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.send('Hello Wor ld!')
-});
-
 app.listen(3000, function () {
 });
 
-app.post('/cafe', function (req, res) {
-    if (req.body.text == 1 || req.body.text == 3 || req.body.text == 8) {
+var food_options = {"vg": 4, "v": 1, "ng": 9}
 
+app.post('/cafe', function (req, res) {
+
+    parts = req.body.text.split("-");
+    cafe_options = parts[1];
+    building_nr = parts[0].trim();
+    console.log(cafe_options);
+    if (building_nr == 1 || building_nr == 3 || building_nr == 8) {
+        console.log(parts);
         var mes = {"attachments": []};
-        for (var meal_type in cafe[req.body.text]) {
+        for (var meal_type in cafe[building_nr]) {
             var att = {
                 "color": ((meal_type == "Breakfast") ? "E4981E" : "1DC47C"),
-                "author_name": "Café " + req.body.text,
+                "author_name": "Café " + building_nr,
                 "title": meal_type,
-                "title_link": "http://sap.cafebonappetit.com/cafe/cafe-" + req.body.text + "/#panel-daypart-menu-" + ((meal_type == "Breakfast") ? "1" : "2"),
+                "title_link": "http://sap.cafebonappetit.com/cafe/cafe-" + building_nr + "/#panel-daypart-menu-" + ((meal_type == "Breakfast") ? "1" : "2"),
                 "fields": []
             };
 
-            for (var i in cafe[req.body.text][meal_type]) {
-                var meal = cafe[req.body.text][meal_type][i];
-                att.fields.push({
-                    "title": meal.label + ((meal.price != "")? " ("+meal.price+")":""),
-                    "value": meal.description.replace(/<br \/>/g, "\n"),
-                    "short": false
-                });
+            for (var i in cafe[building_nr][meal_type]) {
+
+                var meal = cafe[building_nr][meal_type][i];
+
+                if (cafe_options !== undefined) {
+                    if (food_options[cafe_options] in meal.cor_icon) {
+                        att.fields.push(renderField(meal));
+                    }
+                }
+                else {
+                    att.fields.push(renderField(meal));
+                }
             }
             mes.attachments.push(att);
         }
@@ -88,14 +96,30 @@ app.post('/cafe', function (req, res) {
 });
 
 app.post('/wheretoeat', function (req, res) {
-    if (req.body.text.length >= 3) {
+
+    parts = req.body.text.split("-");
+    cafe_options = parts[1];
+    query = parts[0].trim();
+    if (query.length >= 3) {
         var hit_cafes = {};
         for (var i in search_index) {
             var entry = search_index[i];
-            if (entry.item.label.toLowerCase().includes(req.body.text.toLowerCase()) || entry.item.description.toLowerCase().includes(req.body.text.toLowerCase())) {
-                hit_cafes[entry.cafe] = hit_cafes[entry.cafe] || [];
-                hit_cafes[entry.cafe][entry.meal_type] = hit_cafes[entry.cafe][entry.meal_type] || [];
-                hit_cafes[entry.cafe][entry.meal_type].push(entry.item);
+            if (entry.item.label.toLowerCase().includes(query.toLowerCase()) || entry.item.description.toLowerCase().includes(query.toLowerCase())) {
+                if (cafe_options !== undefined) {
+                    if (food_options[cafe_options] in entry.item.cor_icon) {
+                        hit_cafes[entry.cafe] = hit_cafes[entry.cafe] || [];
+                        hit_cafes[entry.cafe][entry.meal_type] = hit_cafes[entry.cafe][entry.meal_type] || [];
+                        hit_cafes[entry.cafe][entry.meal_type].push(entry.item);
+
+                    }
+
+
+                }
+                else {
+                    hit_cafes[entry.cafe] = hit_cafes[entry.cafe] || [];
+                    hit_cafes[entry.cafe][entry.meal_type] = hit_cafes[entry.cafe][entry.meal_type] || [];
+                    hit_cafes[entry.cafe][entry.meal_type].push(entry.item);
+                }
             }
         }
         console.log(hit_cafes);
@@ -112,11 +136,7 @@ app.post('/wheretoeat', function (req, res) {
                     };
                     for (var item_index in hit_cafes[cafe][meal_type]) {
                         var meal = hit_cafes[cafe][meal_type][item_index];
-                        att.fields.push({
-                            "title": meal.label + ((meal.price != "")? " ("+meal.price+")":""),
-                            "value": meal.description.replace(/<br \/>/g, "\n"),
-                            "short": false
-                        });
+                        att.fields.push(renderField(meal));
                     }
                     mes.attachments.push(att);
                 }
@@ -142,4 +162,22 @@ app.post('/wheretoeat', function (req, res) {
 });
 
 
+function renderField(meal) {
+    var icon_string = "";
+    if (4 in meal.cor_icon) {
+        icon_string += ":vegan:";
+    }
+    if (1 in meal.cor_icon) {
+        icon_string += ":vegetarian:";
+    }
+    if (9 in meal.cor_icon) {
+        icon_string += ":nogluten:";
+    }
+    return {
+        "title": meal.label + ((meal.price != "") ? " (" + meal.price + ")" : "") + icon_string,
+        "value": meal.description.replace(/<br \/>/g, "\n"),
+        "short": false
+    };
 
+
+}
